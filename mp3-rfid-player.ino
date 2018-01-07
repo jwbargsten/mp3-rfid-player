2d00e1f34b9e939af64d473c3bc643f23f18a28c
Joachim Bargsten <jw@bargsten.org>
2018-01-07 22:15:46 +0100

    tags from file

--- mp3-rfid-player.ino
+++ mp3-rfid-player.ino
@@ -2,22 +2,93 @@
 #include <SdFat.h>
 #include <FreeStack.h>
 #include <SFEMP3Shield.h>
+#include <stdio.h>
+#include <string.h>                      
+#include <stdlib.h>     
 
 SdFat sd;
 SFEMP3Shield MP3player;
 
-const int nKnownTags = 6;
-char* authorizedTags[nKnownTags];
+const char SEP_CHAR = ':';               
+const int TAGS_MAX = 100;                
+char* authorizedTags[TAGS_MAX];          
+const size_t LINE_DIM = 30;              
 
-void initAuthorizedTags() {
-  authorizedTags[0] = "4700658674D0";
-  authorizedTags[1] = "470065C46680";
-  authorizedTags[2] = "4A0096EEDDEF";
-  authorizedTags[3] = "010A3B6E9CC2";
-  authorizedTags[4] = "010A3ACE56A9";
-  authorizedTags[5] = "010A3B621F4D";
-}
 
+int read_tags
+{ 
+  SdFile file;
+
+  if (!file.open("TAGS.TXT", O_READ)) error("open failed");
+  FILE * file;                           
+  char line[LINE_DIM];                   
+  int n;
+  int tlen = 0;                          
+  char *tstart, *lend, *tmp;             
+
+  for(int i = 0; i < TAGS_MAX; i++)      
+    authorizedTags[i] = NULL;            
+
+
+  file = fopen ("TAGS.TXT" , "r");       
+  if (file == NULL) perror ("Error opening file");
+
+  puts(".");
+  while (fgets(line, sizeof(line), file) != NULL) {
+    tstart = strchr(line, SEP_CHAR);     
+    lend = strchr(line, '\n');           
+    if(tstart && lend) {                 
+      tstart++;                          
+      if(lend - 1 == '\r') lend--;       
+      int idx = atoi(line);              
+      fprintf(stderr,"idx: %d ", idx);   
+      if(idx >= TAGS_MAX || idx < 0)     
+        continue;
+      tlen = lend - tstart;              
+      fprintf(stderr, "idx: %d, tlen: %d\n", idx, tlen);
+      fflush(stdout);                    
+      authorizedTags[idx] = strndup(tstart, tlen * sizeof(char));
+    }
+  }
+  for(int i = 0; i < TAGS_MAX; i++) {    
+    if(authorizedTags[i] == NULL) {      
+      printf("idx %d: empty\n", i);      
+    } else {                             
+      printf("idx %d: %s\n", i, authorizedTags[i]);
+    }
+  }
+  fclose (file);                         
+  return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void initAuthorizedTags() {
+//  authorizedTags[0] = "4700658674D0";
+//  authorizedTags[1] = "470065C46680";
+//  authorizedTags[2] = "4A0096EEDDEF";
+//  authorizedTags[3] = "010A3B6E9CC2";
+//  authorizedTags[4] = "010A3ACE56A9";
+//  authorizedTags[5] = "010A3B621F4D";
+//}
+
+ const size_t LINE_DIM = 30;
 void setup() {
   uint8_t result;
 
@@ -25,7 +96,36 @@ void setup() {
 
   if (!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
   if (!sd.chdir("/")) sd.errorHalt("sd.chdir");
+//hier authorizedTags fuellen
+  if (!file.open("TAGS.TXT", O_READ)) error("open failed");
+  
 
+
+  char line[LINE_DIM];
+
+  while ((n = file.fgets(line, sizeof(line))) > 0) {
+    const char *tstart = strchr(line, sep);
+    
+    const char *lend = strchr(line, '\n');
+if(tstart && lend) {
+  tstart++;
+  if(lend - 1 == '\r')
+    lend--;
+  int idx = atoi(line);
+  malloc((lend-tstart) * sizeof(int));
+   int index = ptr - values;
+   // do something
+}
+      
+      int clidx = strchr(
+      if (line[n - 1] != '\n') {
+        // Line is too long or last line is missing nl.
+        Serial.println(F(" <-- missing nl"));
+      }
+  }
+  file.close();
+
+  
   result = MP3player.begin();
 
   MP3player.setVolume(10, 10);
